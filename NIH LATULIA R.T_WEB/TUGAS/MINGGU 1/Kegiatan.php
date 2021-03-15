3041924df9c553423741882c19a2b250e99bc0d1
Sintanwd <71804430+Sintanwd@users.noreply.github.com>
2021-03-15 18:50:36 +0700

    UP KEGIATAN

---
+++ NIH LATULIA R.T_WEB/TUGAS/MINGGU 1/Kegiatan.php
@@ -0,0 +1,50 @@
+<?php
+
+//Autor Nih Latulia R.T - E31190397 - GOL A
+
+// declare class
+class Car {
+	//properties
+	public $comp;
+	public $color ='beige';
+	public $hasSunRoof = true;
+
+	//method = hellow
+	public function hellow()
+	{
+		return "beep";
+	}
+} 
+
+//membuat instance
+$bmw = new Car ();
+$mercedes = new Car ();
+
+// get values
+echo $bmw -> color; //beige
+echo "<br />";
+echo $mercedes -> color; //beige
+echo "<br />";
+
+// set values
+$bmw -> color = 'blue';
+$bmw -> comp = "BMW";
+$mercedes -> comp = "Mercedes Benz";
+
+//get values 
+echo $bmw -> color; //blue
+echo "<br />";
+echo $mercedes -> color; //blue
+echo "<br />";
+echo $bmw -> comp; // BMW
+echo "<br />";
+echo $mercedes -> comp; // MERCEDES BENZ
+echo "<br />";
+
+//method get a beep
+echo $bmw -> hellow(); //beep
+echo "<br />";
+echo $mercedes -> hellow(); //beep
+
+
+ ?>
